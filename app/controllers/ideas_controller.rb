3c508f9a0643609e2949fb1c80c218b87047092a
Your Name <you@example.com>
2015-02-09 22:34:52 -0800

    implemented CREATE function, also created a partial which gets redirected to when new.html.erb is called

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -8,7 +8,16 @@ class IdeasController < ApplicationController
   def new
     # render text: "create a new project"
     @idea = Idea.new
-    
   end
+
+  def create
+    @idea = Idea.new(params.require(:idea).permit(:title, :explanation))
+    if @idea.save
+    redirect_to root_path, notice: "idea created!"
+    else
+    redirect_to root_path, notice: "cant create idea, something went wrong :/"
+    end
+  end
+
 
 end
