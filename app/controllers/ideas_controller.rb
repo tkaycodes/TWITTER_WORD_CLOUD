ed7b5ea123d261a49ee70293d8782b0825d5ee81
Your Name <you@example.com>
2015-02-09 18:10:11 -0800

    added LOGO image using image_tag, added logout and login button, added hello user text, added views in the index action of the ideas controller displaying all the current ideas

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -2,6 +2,11 @@ class IdeasController < ApplicationController
 
   def index
     # render text: "hello world"
+    @ideas = Idea.all
+  end
+
+  def new
+    # render text: "create a new project"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root "ideas#index"
+  resources :ideas
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
