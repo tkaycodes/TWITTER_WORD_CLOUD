0543f41900cad461168052c0c6e246c78a99eb02
Your Name <you@example.com>
2015-02-26 10:15:38 -0800

    added corresponding routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   devise_for :users,
     controllers: {omniauth_callbacks:"users/omniauth_callbacks"}
   root "ideas#index"
-  resources :ideas
+  resources :ideas do
+    resources :pledges, only: [:new, :create, :destroy, :index] 
+  end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
