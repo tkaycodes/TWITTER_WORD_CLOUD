9c9f5c6db35b1e894d81c6ca6770e422becdff5c
Your Name <you@example.com>
2015-03-31 18:07:13 -0700

    updated location of dev keys

--- app/models/twitterclient.rb
+++ app/models/twitterclient.rb
@@ -2,10 +2,10 @@ class Twitterclient
 
   def self.client
     @client ||= Twitter::REST::Client.new do |config|;
-      config.consumer_key = "4QhFWrHOHti3NCpzyXWPEWH9S";
-      config.consumer_secret = "kltQq8AmwSSoNigNlk7MUPqM5zWPrNCR1cgfxhlWwlKJbWFZ19";
-      config.access_token = "398471310-RES7sRpSpJWZ5LZLiCUUHiaGKRO3QjNCDqy36sv0";
-      config.access_token_secret = "ayBE0gLPxHrYaTSLtdJbITQSUPRTf0PBzhpBn4yxcrBcu";
+      config.consumer_key = ENV["TWITTER_API_KEY"];
+      config.consumer_secret = ENV["TWITTER_API_SECRET"];
+      config.access_token = ENV["TWITTER_ACCESS_TOKEN"];
+      config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"];
     end
   end
 
